94dbee578aa90322b054780b32ac31badd89d2a4
K.V. <30656341+KVonGit@users.noreply.github.com>
2025-04-04 01:24:54 -0500

    Add Chauncey

--- commands/utility/petcheeto.js
+++ commands/utility/petcheeto.js
@@ -8,6 +8,6 @@ module.exports = {
         // Send a message into the channel where command was triggered from
         const cheetoResponses = ['smiles', 'purrs', 'rolls over', 'looks at you funny', 'moves just out of reach', 'falls asleep', 'farts', 'paws at you playfully', 'scratches you, but not too hard', 'is busy playing with someone under the door', 'lies down on your hand', 'goes to the litter box', 'goes and eats some food', 'walks away to have a little water', 'stands by the door, waiting to go out, looking at you upside-down', 'makes biscuits', 'loves you'];
         const random = cheetoResponses[Math.floor(Math.random() * cheetoResponses.length)];
-        await interaction.reply('Cheeto ' + random + '.');
+        await interaction.reply({ content: 'Cheeto ' + random + '.', flags: 64 });
     },
 };
